94bb70ba65b8a9084f316e0cf3214539d9d71dde
BrooksW82 <35711130+BrooksW82@users.noreply.github.com>
2019-12-26 22:32:13 -0600

    Add files via upload

--- BaseTests.java
+++ BaseTests.java
@@ -1,8 +1,12 @@
 package base;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.List;
 
 public class BaseTests {
 
@@ -13,10 +17,14 @@ public class BaseTests {
         driver = new ChromeDriver();
         driver.get("https://the-internet.herokuapp.com/");
 
-        driver.manage().window().setSize(new Dimension(375,812));
+        List<WebElement> links = driver.findElements(By.tagName("a"));
+        System.out.println(links.size());
+
+        WebElement inputsLink = driver.findElement(By.linkText("Inputs"));
+        inputsLink.click();
 
         System.out.println(driver.getTitle());
-        //driver.quit();
+        driver.quit();
     }
 
     public static void main(String args[]){
