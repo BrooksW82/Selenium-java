d69d32b524b56f7a8f3eca03dceef4ad966b33ac
BrooksW82 <35711130+BrooksW82@users.noreply.github.com>
2019-12-30 22:38:43 -0600

    Add files via upload
    foundational classes expanded through different pages for testing.

--- BaseTests.java
+++ BaseTests.java
@@ -5,28 +5,22 @@ import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import pages.HomePage;
 
 import java.util.List;
 
 public class BaseTests {
 
     private WebDriver driver;
+    protected HomePage homePage;
 
     public void setUp(){
         System.setProperty("webdriver.chrome.driver", "resources/chromedriver.exe");
         driver = new ChromeDriver();
         driver.get("https://the-internet.herokuapp.com/");
 
+        homePage = new HomePage(driver);
 
-        WebElement inputsLink = driver.findElement(By.linkText("Shifting Content"));
-        inputsLink.click();
-
-        inputsLink = driver.findElement(By.linkText("Example 1: Menu Element"));
-        inputsLink.click();
-
-        List<WebElement> links = driver.findElements(By.tagName("li"));
-        System.out.println(links.size());
-        System.out.println(driver.getTitle());
         //driver.quit();
     }
 
