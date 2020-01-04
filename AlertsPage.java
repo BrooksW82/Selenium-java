bc0d2d77bee1a62015ed3479e2d4fa3d290143e2
BrooksW82 <35711130+BrooksW82@users.noreply.github.com>
2020-01-04 16:18:48 -0600

    Add files via upload

--- AlertsPage.java
+++ AlertsPage.java
@@ -8,7 +8,7 @@ public class AlertsPage {
     private WebDriver driver;
     private By triggerAlertButton = By.xpath(".//button[text() = 'Click for JS Alert']");
     private By triggerConfirmButton = By.xpath(".//button [text() = 'Click for JS Confirm']");
-
+    private By triggerPromptButton = By.xpath(".//button [text() = 'Click for JS Prompt']");
     private By results = By.id("result");
 
     public AlertsPage(WebDriver driver){
@@ -23,6 +23,9 @@ public class AlertsPage {
         driver.findElement(triggerConfirmButton).click();
     }
 
+    public void triggerPrompt(){
+        driver.findElement(triggerPromptButton).click();
+    }
 
 
     public void alert_clickToAccept(){
@@ -35,6 +38,10 @@ public class AlertsPage {
 
     public String alert_getText(){
         return driver.switchTo().alert().getText();
+    }
+
+    public void alert_setInput(String text){
+        driver.switchTo().alert().sendKeys(text);
     }
 
     public String getResult(){
