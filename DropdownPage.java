44a108661512251a48393db048ceed30ed974b0f
BrooksW82 <35711130+BrooksW82@users.noreply.github.com>
2020-01-08 17:18:15 -0600

    Add files via upload

--- DropdownPage.java
+++ DropdownPage.java
@@ -1,6 +1,7 @@
 package pages;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.Select;
@@ -31,9 +32,10 @@ public class DropdownPage {
         return new Select(driver.findElement(dropdown));
     }
 
-
-    public void selectMultipleOptions(){
-        
+    public void addMultipleOptions(){
+        WebElement dropDownElement = driver.findElement(dropdown);
+        String script = "arguments[0].setAttribute('multiple','')";
+        ((JavascriptExecutor)driver).executeScript(script, findDropDownElement());
     }
 }
 
--- JavaScriptTests.java
+++ JavaScriptTests.java
@@ -2,6 +2,11 @@ package javascript;
 
 import base.BaseTests;
 import org.testng.annotations.Test;
+import pages.DropdownPage;
+
+import java.util.List;
+
+import static org.testng.Assert.*;
 
 public class JavaScriptTests extends BaseTests {
 
@@ -13,5 +18,18 @@ public class JavaScriptTests extends BaseTests {
     @Test
     public void testScrollToFifthParagraph(){
         homePage.clickInfiniteScroll().scrollToParagraph(5);
+    }
+
+    @Test
+    public void testSelectMultipleDropdown(){
+        var dropdown = homePage.clickDropDown();
+        dropdown.addMultipleOptions();
+
+        var optionsToSelect = List.of("Option 1", "Option 2");
+        optionsToSelect.forEach(dropdown::selectFromDropDown);
+
+        var selectedOptions = dropdown.getSelectedOptions();
+        assertTrue(selectedOptions.containsAll(optionsToSelect), "All options not selected");
+        assertEquals(selectedOptions.size(), optionsToSelect.size(), "Number of selected items incorrect");
     }
 }
