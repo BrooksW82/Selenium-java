97abd58bf0eb2400a7982c47e8ef97211193434a
BrooksW82 <35711130+BrooksW82@users.noreply.github.com>
2020-01-09 13:57:44 -0600

    Add files via upload

--- BaseTests.java
+++ BaseTests.java
@@ -1,11 +1,21 @@
 package base;
 
+import com.google.common.io.Files;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.ITestResult;
 import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import pages.HomePage;
+import utils.WindowManager;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
 
 public class BaseTests {
 
@@ -16,17 +26,36 @@ public class BaseTests {
     public void setUp(){
         System.setProperty("webdriver.chrome.driver", "resources/chromedriver.exe");
         driver = new ChromeDriver();
+        //driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
         goHome();
-        homePage = new HomePage(driver);
     }
 
     @BeforeMethod
     public void goHome(){
         driver.get("https://the-internet.herokuapp.com/");
+        homePage = new HomePage(driver);
     }
 
     @AfterClass
-    public void tearDown(){
+    public void tearDown() {
         driver.quit();
+    }
+
+    @AfterMethod
+    public void recordFailure(ITestResult result){
+        if(ITestResult.FAILURE == result.getStatus()){
+            var camera = (TakesScreenshot)driver;
+            File screenshot = camera.getScreenshotAs(OutputType.FILE);
+            try{
+                Files.move(screenshot, new File("resources/screenshots/"
+                        + result.getName() + ".png"));
+            }catch (IOException e){
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public WindowManager getWindowManager(){
+        return new WindowManager(driver);
     }
 }
